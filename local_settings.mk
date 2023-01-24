fa90cd64cbc21597cf041e211879271b07713e1b
carlodefalco <carlodefalco@users.noreply.github.com>
2023-01-24 12:47:54 +0100

    commented out options specific for my local system

--- local_settings.mk
+++ local_settings.mk
@@ -1,12 +1,12 @@
-CXX=mpicxx
+# CXX=mpicxx
 
-CXXFLAGS= -std=c++17 -O2 -mmacosx-version-min=11.6 -Wno-unused-command-line-argument
+# CXXFLAGS= -std=c++17 -O2 -mmacosx-version-min=11.6 -Wno-unused-command-line-argument
 
-CPPFLAGS=-I. -I/opt/bim/0.0.1/include -I/opt/p4est/dev/include -I/opt/local/include -I/opt/lis/2.0.31/include -I/u/software/octave_file_io/1.0.91/include/ \
--DHAVE_OCTAVE_44 -DOMPI_SKIP_MPICXX -DBIM_TIMING -DUSE_MPI
+# CPPFLAGS=-I. -I/opt/bim/0.0.1/include -I/opt/p4est/dev/include -I/opt/local/include -I/opt/lis/2.0.31/include -I/u/software/octave_file_io/1.0.91/include/ \
+# -DHAVE_OCTAVE_44 -DOMPI_SKIP_MPICXX -DBIM_TIMING -DUSE_MPI
 
-LDFLAGS=-Wl,-no_compact_unwind -mmacosx-version-min=11.6 -O3 -Wl,-rpath -Wl,/opt/local/lib/gcc12/ \
--L/opt/bim/0.0.1/lib -L/opt/local/lib -L/opt/octave_file_io/1.0.91/lib/ -L/opt/lis/2.0.31/lib -L/opt/p4est/dev/lib
+# LDFLAGS=-Wl,-no_compact_unwind -mmacosx-version-min=11.6 -O3 -Wl,-rpath -Wl,/opt/local/lib/gcc12/ \
+# -L/opt/bim/0.0.1/lib -L/opt/local/lib -L/opt/octave_file_io/1.0.91/lib/ -L/opt/lis/2.0.31/lib -L/opt/p4est/dev/lib
 
-LIBS=-lbim -lbimmumps -lbimlis -lbimp4est -lbimlinalg -llis -L/opt/local/lib -lptscotch -lscotch -lmpi -Wl,-flat_namespace -Wl,-commons,use_dylibs \
--L/opt/local/lib/openmpi-mp -lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lopenblas -L/opt/local/lib/gcc12/ -lgfortran
+# LIBS=-lbim -lbimmumps -lbimlis -lbimp4est -lbimlinalg -llis -L/opt/local/lib -lptscotch -lscotch -lmpi -Wl,-flat_namespace -Wl,-commons,use_dylibs \
+# -L/opt/local/lib/openmpi-mp -lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi -lopenblas -L/opt/local/lib/gcc12/ -lgfortran
